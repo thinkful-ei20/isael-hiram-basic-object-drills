cffa16c3fb92e34c4c452e9fd284a806f8149625
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-04-04 12:18:33 -0600

    finished object updater

---
+++ objectUpdater.js
@@ -0,0 +1,9 @@
+'use strict';
+function updateObject(obj) {
+  obj['foo'] = 'foo';
+  obj['bar'] = 'bar';
+  obj['bizz'] = 'bizz';
+  obj['bang'] = 'bang';
+  return obj;
+}
+console.log(updateObject({}));
